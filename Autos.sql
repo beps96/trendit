6ce8e579f411e452adbb1dce189f5c041da083b6
Arturo <chulorabo@gmail.com>
2019-11-22 22:14:06 -0600

    Se modifica query con unions de Autos

--- Autos.sql
+++ Autos.sql
@@ -1,45 +1,76 @@
 -- CIFRAS CONTROL PREVIOS
-
-SELECT 'bddlapr.aut_insumos_sb1_emision_poliza' tabla, COUNT (1) registros
+SELECT 'bddlapr.aut_insumos_sb1_emision_poliza' tabla,
+       CONCAT (CAST (format_number (COUNT (1), 0) AS STRING),
+               ' Registros'
+              ) cifras
   FROM bddlapr.aut_insumos_sb1_emision_poliza
 UNION ALL
-SELECT 'bddlapr.aut_insumos_sb1_emision_cobertura' tabla, COUNT (1) registros
+SELECT 'bddlapr.aut_insumos_sb1_emision_cobertura' tabla,
+       CONCAT (CAST (format_number (COUNT (1), 0) AS STRING),
+               ' Registros'
+              ) cifras
   FROM bddlapr.aut_insumos_sb1_emision_cobertura
 UNION ALL
-SELECT 'bddlapr.aut_siniestro_poliza' tabla, COUNT (1) registros
+SELECT 'bddlapr.aut_siniestro_poliza' tabla,
+       CONCAT (CAST (format_number (COUNT (1), 0) AS STRING),
+               ' Registros'
+              ) cifras
   FROM bddlapr.aut_siniestro_poliza
 UNION ALL
-SELECT 'bddlapr.aut_siniestro_cob_afecta' tabla, COUNT (1) registros
+SELECT 'bddlapr.aut_siniestro_cob_afecta' tabla,
+       CONCAT (CAST (format_number (COUNT (1), 0) AS STRING),
+               ' Registros'
+              ) cifras
   FROM bddlapr.aut_siniestro_cob_afecta;
 
 
 -- PASO 1 Actualizar Elementos a nivel póliza, objeto y cobertura
-
-SELECT 'bddlalm.aut_elemento_poliza_tp' tabla, COUNT (1) registros
+SELECT 'bddlalm.aut_elemento_poliza_tp' tabla,
+       CONCAT (CAST (format_number (COUNT (1), 0) AS STRING),
+               ' Registros'
+              ) cifras
   FROM bddlalm.aut_elemento_poliza_tp
 UNION ALL
-SELECT 'bddlalm.aut_elemento_objeto_tp' tabla, COUNT (1) registros
-  FROM bddlalm.aut_elemento_objeto_tp
-UNION ALL
-SELECT 'bddlalm.aut_elemento_cobertura_tp' tabla, COUNT (1) registros
-  FROM bddlalm.aut_elemento_cobertura_tp;
-
-
-
-SELECT 'bddlalm.aut_elemento_poliza_tp' tabla, MAX (tsultmod) fechamax
+SELECT 'bddlalm.aut_elemento_poliza_tp' tabla,
+       CONCAT ('Fecha Maxima: ',
+               CAST (MAX (ts_ultima_modificacion) AS STRING)
+              ) cifras
   FROM bddlalm.aut_elemento_poliza_tp
 UNION ALL
-SELECT 'bddlalm.aut_elemento_objeto_tp' tabla, MAX (tsultmod) fechamax
+SELECT 'bddlalm.aut_elemento_objeto_tp' tabla,
+       CONCAT (CAST (format_number (COUNT (1), 0) AS STRING),
+               ' Registros'
+              ) cifras
   FROM bddlalm.aut_elemento_objeto_tp
 UNION ALL
-SELECT 'bddlalm.aut_elemento_cobertura_tp' tabla, MAX (tsultmod) fechamax
-  FROM bddlalm.aut_elemento_cobertura_tp;
+SELECT 'bddlalm.aut_elemento_objeto_tp' tabla,
+       CONCAT ('Fecha Maxima: ',
+               CAST (MAX (ts_ultima_modificacion) AS STRING)
+              ) cifras
+  FROM bddlalm.aut_elemento_objeto_tp
+UNION ALL
+SELECT 'bddlalm.aut_elemento_cobertura_tp' tabla,
+       CONCAT (CAST (format_number (COUNT (1), 0) AS STRING),
+               ' Registros'
+              ) cifras
+  FROM bddlalm.aut_elemento_cobertura_tp
+UNION ALL
+SELECT 'bddlalm.aut_elemento_cobertura_tp' tabla,
+       CONCAT ('Fecha Maxima: ',
+               CAST (MAX (ts_ultima_modificacion) AS STRING)
+              ) cifras
+  FROM bddlalm.aut_elemento_cobertura_tp;  
 
 -- PASO 2 Actualizar la tabla Aut Poliza
 
-SELECT 'bddlalm.aut_poliza' tabla, COUNT (1) registros
-  FROM bddlalm.aut_poliza;
-
-
-SELECT 'bddlalm.aut_poliza' tabla, MAX (ts_ultima_modificacion) fechamax
+SELECT 'bddlalm.aut_poliza' tabla,
+       CONCAT (CAST (format_number (COUNT (1), 0) AS STRING),
+               ' Registros'
+              ) cifras
+  FROM bddlalm.aut_poliza
+UNION ALL
+SELECT 'bddlalm.aut_poliza' tabla,
+       CONCAT ('Fecha Maxima: ',
+               CAST (MAX (ts_ultima_modificacion) AS STRING)
+              ) cifras
   FROM bddlalm.aut_poliza;
